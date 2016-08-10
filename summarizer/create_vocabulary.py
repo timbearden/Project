2d565e14228190b70df4ea0483550acbee433b7d
Tim Bearden <Tbearden89@gmail.com>
2016-08-10 09:21:27 -0700

    Updates vocabulary

--- summarizer/create_vocabulary.py
+++ summarizer/create_vocabulary.py
@@ -1,29 +1,33 @@
 from pymongo import MongoClient
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from nltk import sent_tokenize, word_tokenize
+import pandas as pd
 import pickle
 
-mongo_client = MongoClient()
-db = mongo_client.g_project_data
-coll = db.test_data
+# mongo_client = MongoClient()
+# db = mongo_client.g_project_data
+# coll = db.test_data
+#
+# summary_list = []
+# article_list = []
+# for doc in list(coll.find()):
+#     if doc['full_text'] != ' ':
+#         summary_list.append(doc['summary'])
+#         article_list.append(doc['full_text'])
+#
+# for i in xrange(len(article_list)):
+#     text = ''
+#     for article in article_list[i]:
+#         text += article
+#     article_list[i] = text
+#
+# summary_test = [summary_list[i] for i in xrange(len(summary_list)) if article_list[i] != '' and sent_tokenize(article_list[i]) > 10]
+# article_test = [article for article in article_list if article != '' and sent_tokenize(article) > 10]
+#
+# mongo_client.close()
 
-summary_list = []
-article_list = []
-for doc in list(coll.find()):
-    if doc['full_text'] != ' ':
-        summary_list.append(doc['summary'])
-        article_list.append(doc['full_text'])
-
-for i in xrange(len(article_list)):
-    text = ''
-    for article in article_list[i]:
-        text += article
-    article_list[i] = text
-
-summary_test = [summary_list[i] for i in xrange(len(summary_list)) if article_list[i] != '' and sent_tokenize(article_list[i]) > 10]
-article_test = [article for article in article_list if article != '' and sent_tokenize(article) > 10]
-
-mongo_client.close()
+articles = pd.read_csv('../data/articles.csv')
+article_list = articles.body
 
 tfidf = TfidfVectorizer(stop_words='english')
 tfidf.fit(article_list)
