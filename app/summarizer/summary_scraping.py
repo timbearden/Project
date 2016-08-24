eb2310db68819efa5ed2c23bc5d0637a95a6a448
Tim Bearden <Tbearden89@gmail.com>
2016-08-23 19:44:02 -0700

    Does other stuff

--- app/summarizer/summary_scraping.py
+++ app/summarizer/summary_scraping.py
@@ -1,7 +1,7 @@
 import requests
 import threading
 from bs4 import BeautifulSoup
-from newspaper3k import Article
+from newspaper import Article
 from pymongo import MongoClient
 from time import sleep
 
